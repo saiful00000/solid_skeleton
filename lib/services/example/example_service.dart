ed25f551dca783fed056eef2269f5ca3a1a0abf4
shaiful islam <md.shaiful@apsissolutions.com>
2023-11-29 00:36:23 +0600

    Example service added

---
+++ lib/services/example/example_service.dart
@@ -0,0 +1,13 @@
+import 'i_example_service.dart';
+
+class ExampleService implements IExampleService {
+  @override
+  void printExample() {
+    // TODO: implement printExample
+  }
+
+  @override
+  void sayHi() {
+    // TODO: implement sayHi
+  }
+}
